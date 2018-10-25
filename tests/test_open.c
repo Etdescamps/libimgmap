106553b2d65ccf2343de966108b9c07bc34e8ae7
Etienne Descamps <16402136+Etdescamps@users.noreply.github.com>
2018-10-25 12:55:48 +0200

    test_open.c: test if a file is correctly opened

--- tests/test_open.c
+++ tests/test_open.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "imgmap.h"
 
 int main(int argc, char **argv) {
-  return 0;
+  if(argc<2)
+    return 1;
+  IMGMAP_FILE fmap;
+  int ret;
+  if((ret = imgmap_open(&fmap, argv[1], IMGMAP_READPRIVATE))<0) {
+    fprintf(stderr, "Error %d while openind %s\n", ret, argv[1]);
+    return ret;
+  }
+  printf("%s: %d %d %d %d %d\n", argv[1], fmap.data_type, fmap.sx, fmap.sy,
+      fmap.max_val, fmap.nc);
+  return imgmap_close(&fmap);
 }
 
